dc3e515b3afbb2cd9d8dd6041b070cb35a158326
r0nsha <r0nsh4v1t@gmail.com>
2025-09-20 19:46:15 +0300

    feat: add fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,50 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [],
+    },
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.fontshare(),
+                name: "Clash Display",
+                cssVariable: "--font-header",
+            },
+            {
+                provider: fontProviders.fontshare(),
+                name: "Satoshi",
+                cssVariable: "--font-body",
+            },
+            {
+                provider: "local",
+                name: "Iosevka",
+                cssVariable: "--font-mono",
+                variants: [
+                    {
+                        weight: 400,
+                        style: "normal",
+                        src: ["./src/assets/fonts/iosevka-latin-400-normal.woff2"],
+                    },
+                    {
+                        weight: 400,
+                        style: "italic",
+                        src: ["./src/assets/fonts/iosevka-latin-400-italic.woff2"],
+                    },
+                    {
+                        weight: 700,
+                        style: "normal",
+                        src: ["./src/assets/fonts/iosevka-latin-700-normal.woff2"],
+                    },
+                    {
+                        weight: 700,
+                        style: "italic",
+                        src: ["./src/assets/fonts/iosevka-latin-700-italic.woff2"],
+                    },
+                ],
+            },
+        ],
     },
 });
