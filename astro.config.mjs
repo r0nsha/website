ece372405abf436b53800376b13b9ea755858857
r0nsha <r0nsh4v1t@gmail.com>
2025-09-21 13:09:07 +0300

    fix: Satoshi font

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,51 +5,52 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [],
-  },
+    vite: {
+        plugins: [],
+    },
 
-  experimental: {
-      fonts: [
-          {
-              provider: fontProviders.bunny(),
-              name: "Satoshi",
-              cssVariable: "--font-default",
-          },
-          {
-              provider: fontProviders.bunny(),
-              name: "Space Grotesk",
-              cssVariable: "--font-header",
-          },
-          {
-              provider: "local",
-              name: "Iosevka",
-              cssVariable: "--font-mono",
-              variants: [
-                  {
-                      weight: 400,
-                      style: "normal",
-                      src: ["./src/assets/fonts/iosevka-latin-400-normal.woff2"],
-                  },
-                  {
-                      weight: 400,
-                      style: "italic",
-                      src: ["./src/assets/fonts/iosevka-latin-400-italic.woff2"],
-                  },
-                  {
-                      weight: 700,
-                      style: "normal",
-                      src: ["./src/assets/fonts/iosevka-latin-700-normal.woff2"],
-                  },
-                  {
-                      weight: 700,
-                      style: "italic",
-                      src: ["./src/assets/fonts/iosevka-latin-700-italic.woff2"],
-                  },
-              ],
-          },
-      ],
-  },
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.fontshare(),
+                name: "Satoshi",
+                cssVariable: "--font-default",
+            },
+            {
+                provider: fontProviders.bunny(),
+                name: "Space Grotesk",
+                cssVariable: "--font-header",
+            },
+            {
+                provider: "local",
+                name: "Iosevka",
+                cssVariable: "--font-mono",
+                variants: [
+                    {
+                        weight: 400,
+                        style: "normal",
+                        src: ["./src/assets/fonts/iosevka-latin-400-normal.woff2"],
+                    },
+                    {
+                        weight: 400,
+                        style: "italic",
+                        src: ["./src/assets/fonts/iosevka-latin-400-italic.woff2"],
+                    },
+                    {
+                        weight: 700,
+                        style: "normal",
+                        src: ["./src/assets/fonts/iosevka-latin-700-normal.woff2"],
+                    },
+                    {
+                        weight: 700,
+                        style: "italic",
+                        src: ["./src/assets/fonts/iosevka-latin-700-italic.woff2"],
+                    },
+                ],
+            },
+        ],
+    },
 
-  integrations: [icon()],
+    integrations: [icon()],
 });
+
