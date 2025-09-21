b87250423270075524f14a9a366770cb19df03f0
r0nsha <r0nsh4v1t@gmail.com>
2025-09-21 03:03:00 +0300

    feat: add links to header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,50 +1,55 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [],
-    },
-    experimental: {
-        fonts: [
-            {
-                provider: fontProviders.bunny(),
-                name: "Satoshi",
-                cssVariable: "--font-default",
-            },
-            {
-                provider: fontProviders.bunny(),
-                name: "Space Grotesk",
-                cssVariable: "--font-header",
-            },
-            {
-                provider: "local",
-                name: "Iosevka",
-                cssVariable: "--font-mono",
-                variants: [
-                    {
-                        weight: 400,
-                        style: "normal",
-                        src: ["./src/assets/fonts/iosevka-latin-400-normal.woff2"],
-                    },
-                    {
-                        weight: 400,
-                        style: "italic",
-                        src: ["./src/assets/fonts/iosevka-latin-400-italic.woff2"],
-                    },
-                    {
-                        weight: 700,
-                        style: "normal",
-                        src: ["./src/assets/fonts/iosevka-latin-700-normal.woff2"],
-                    },
-                    {
-                        weight: 700,
-                        style: "italic",
-                        src: ["./src/assets/fonts/iosevka-latin-700-italic.woff2"],
-                    },
-                ],
-            },
-        ],
-    },
+  vite: {
+      plugins: [],
+  },
+
+  experimental: {
+      fonts: [
+          {
+              provider: fontProviders.bunny(),
+              name: "Satoshi",
+              cssVariable: "--font-default",
+          },
+          {
+              provider: fontProviders.bunny(),
+              name: "Space Grotesk",
+              cssVariable: "--font-header",
+          },
+          {
+              provider: "local",
+              name: "Iosevka",
+              cssVariable: "--font-mono",
+              variants: [
+                  {
+                      weight: 400,
+                      style: "normal",
+                      src: ["./src/assets/fonts/iosevka-latin-400-normal.woff2"],
+                  },
+                  {
+                      weight: 400,
+                      style: "italic",
+                      src: ["./src/assets/fonts/iosevka-latin-400-italic.woff2"],
+                  },
+                  {
+                      weight: 700,
+                      style: "normal",
+                      src: ["./src/assets/fonts/iosevka-latin-700-normal.woff2"],
+                  },
+                  {
+                      weight: 700,
+                      style: "italic",
+                      src: ["./src/assets/fonts/iosevka-latin-700-italic.woff2"],
+                  },
+              ],
+          },
+      ],
+  },
+
+  integrations: [icon()],
 });
