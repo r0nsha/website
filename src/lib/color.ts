37a69e14282c7ff150dc6d18ece88ff518c189d1
r0nsha <r0nsh4v1t@gmail.com>
2025-09-22 15:15:52 +0300

    fix: change default s & l

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -11,12 +11,15 @@ function hslToHex(h: number, s: number, l: number): string {
     return `#${f(0)}${f(8)}${f(4)}`;
 }
 
-export function goldenRatioColor(seed = Math.random(), s = 80, l = 60): string {
+const defaultS = 60;
+const defaultL = 45;
+
+export function goldenRatioColor(seed = Math.random(), s = defaultS, l = defaultL): string {
     const goldenRatio = 0.61803398875;
     const hue = (seed + goldenRatio) % 1;
     return hslToHex(hue * 360, s, l);
 }
 
-export function numberToColor(seed: number, s = 80, l = 60): string {
+export function numberToColor(seed: number, s = defaultS, l = defaultL): string {
     return goldenRatioColor((seed * 10) / 100, s, l);
 }
